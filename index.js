1bd6b321bd53c1e94afa6c90ed305b3bc30e7849
christian99salerno <christian.salerno05@gmail.com>
2018-01-18 12:56:15 +0100

    comment

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ res.end("Ciao");
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express ();
+
 //console.log(path.resolve(__dirname, 'public'));
 
 //app.use( function (req, res, next) {
@@ -21,21 +23,36 @@ const app = express ();
 //next();
 //});
 
+const commenti = [
+{title:"Ciao",
+ body:"come stai?",
+ date:new Date()
+}
+];
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use( morgan('dev') );
+
 app.use( '/js', express.static(path.resolve(__dirname, 'js')));
 app.use( '/css', express.static(path.resolve(__dirname, 'css')));
 app.use( '/img', express.static(path.resolve(__dirname, 'img')));
+
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  //res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  res.render('index');
+app.get('/', function (req, res) {   
+  res.render('index', {commenti: commenti});
 });
 
-app.get('/test', function (req, res) {
-  //res.sendFile(path.resolve(__dirname, 'public', 'test.html'));
-  res.render('test');
+app.get('/comment', function (req, res) {
+  res.render('comment');
+});
+
+app.post('/comment', function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.body);
+  res.redirect('/');
 });
 
 app.use( function (req, res) {
